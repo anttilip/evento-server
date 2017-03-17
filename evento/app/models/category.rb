0f2ca83fae90f87d7b9891ba9aeea519e405482c
anttilip <antti.lip@gmail.com>
2017-03-18 01:49:16 +0200

    Add associations to models

--- evento/app/models/category.rb
+++ evento/app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :events
+  belongs_to :category, optional: true  # Parent
 end
--- evento/app/models/event.rb
+++ evento/app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  belongs_to :category
+  belongs_to :user, :class_name => "User", :foreign_key => 'creator_id'  # Creator
+  has_and_belongs_to_many :users  # Attendees
 end
