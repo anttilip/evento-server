56c745973669dc4801dd27797303a821cfd80098
anttilip <antti.lip@gmail.com>
2017-03-17 18:28:54 +0200

    Add and setup CORS gem

--- evento/config/application.rb
+++ evento/config/application.rb
@@ -26,5 +26,13 @@ module Evento
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        # TODO: These can be changed to match only specific urls like flai.xyz/evento
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
