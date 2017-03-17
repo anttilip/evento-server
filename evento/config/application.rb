711731d25dbf7d9e5ddb44ab31310a8d062fbcea
anttilip <antti.lip@gmail.com>
2017-03-17 18:46:49 +0200

    Fix warning on Rack::Cors config

--- evento/config/application.rb
+++ evento/config/application.rb
@@ -27,7 +27,7 @@ module Evento
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         # TODO: These can be changed to match only specific urls like flai.xyz/evento
         origins '*'
