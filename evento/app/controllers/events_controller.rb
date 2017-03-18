9e77a828684b840c9785bccba86fbc6f1b188092
Antti Lipsanen <antti.lip@gmail.com>
2017-03-18 15:07:11 +0200

    Add eager loading to event#index

--- evento/app/controllers/events_controller.rb
+++ evento/app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
 
   # GET /events
   def index
-    @events = Event.all
+    @events = Event.includes(:user, :category).all
 
     render json: @events
   end
